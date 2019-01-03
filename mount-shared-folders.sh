1cffa7de366e3949d15c4e9db206c7d9becf10f8
root <root@localhost.localdomain>
2019-01-03 09:57:59 -0600

    Updates

--- mount-shared-folders.sh
+++ mount-shared-folders.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
+RED='\033[0;31m'
+NC='\033[0m' # No Color
 vmware-hgfsclient | while read folder; do
-  echo "[i] Mounting Kali-shared   (/mnt/hgfs/Kali-shared)"
+  echo -e "[i] ${RED}Mounting Kali-shared   (/mnt/hgfs/Kali-shared)${NC}"
   mkdir -p "/mnt/hgfs/Kali-shared"
   umount -f "/mnt/hgfs/Kali-shared" 2>/dev/null
-  vmhgfs-fuse -o allow_other -o auto_unmount ".host:/Kali-shared" "/mnt/hgfs/Kali-shared"
+  #vmhgfs-fuse -o allow_other -o auto_unmount ".host:/Kali-shared" "/mnt/hgfs/Kali-shared"
+  vmhgfs-fuse -o allow_other -o auto_unmount ".host:/Kali-shared" "/root/Desktop/Kali-shared"
 done
 
 sleep 2s
